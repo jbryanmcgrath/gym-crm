a390b3d816b7dd368d93e5c8ef34247e37b515b5
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2022-03-08 18:27:41 -0500

    server running backend

--- backend/server/config/connection.js
+++ backend/server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jbryanmcgrath:MongoDB123@cluster0.wkwck.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
