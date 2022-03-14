a534f27496bd290b3fcca7fb9b2feabe50fe206a
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2022-03-14 19:31:50 -0400

    landing page donw. Need to link to login and signup components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './components/Header';
+import PlaceToVisit from './components/InfoGraphics';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundImage: `url(${process.env.Public_URL + '/images/gym-crm.jpg'})`
-  }
-}))
-
-export default function () {
-  const classes = useStyles()
-  return <div className={classes.root}></div>
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/images/gym-crm.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}));
+export default function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Header />
+      <PlaceToVisit />
+    </div>
+  );
 }
