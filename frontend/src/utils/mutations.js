eba1767c084c9f7748b0a8ebbdd21b7d0d1becba
coleleg <coleleg@gmail.com>
2022-03-17 17:47:02 -0400

    add signup/login/addemployee mutations to front end

--- frontend/src/utils/mutations.js
+++ frontend/src/utils/mutations.js
@@ -13,3 +13,25 @@ export const MUTATION_INITIALEMPLOYEE = gql`mutation initialEmployee($firstName:
     }
   }
 `
+export const MUTATION_ADDGYM = gql`mutation addGym($gymName: String!, $gymEmail: String!, $phoneNumber: String!, $address: String!, $city: String!, $zip: String!, $state: String!) {
+  addGym(gymName: $gymName, gymEmail: $gymEmail, phoneNumber: $phoneNumber, address: $address, city: $city, zip: $zip, state: $state) {
+    gymName
+  }
+}`;
+
+export const MUTATION_LOGIN = gql`mutation Login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    employee {
+      _id
+      email
+    }
+  }
+}`;
+
+export const MUTATION_ADDEMPLOYEE = gql`mutation addEmployee($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+  addEmployee(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+    
+  }
+}`;
+
