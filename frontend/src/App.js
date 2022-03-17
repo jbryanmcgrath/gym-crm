f28fd80b43ec16456b9ce1f1c4a5cdab13771f06
coleleg <coleleg@gmail.com>
2022-03-17 17:59:11 -0400

    fix 'ApolloClient has no closing tag' in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,7 @@ export default function App() {
             </Routes>
           </BrowserRouter>
         </GlobalProvider>
-        <ApolloClient />
+        </ApolloClient>
     </div>
   );
 }
