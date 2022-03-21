67e1df0972a99d428327f4293382afd90b5798ed
unknown <dennis11@live.com>
2022-03-21 16:47:33 -0400

    schedular commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,52 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Home from './pages/Home';
-import { Routes, Route, BrowserRouter, useNavigate } from 'react-router-dom';
-import SignInAndCreateUserContainer from './components/SignInAndCreateUserContainer';
-import { GlobalProvider } from './store/GlobalProvider';
-import CustomerTable from './components/CustomerTable'
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import Dashboard from './pages/Dashboard';
-import NewMember from './components/NewMember';
-import AddMember from './pages/AddMember';
-import AuthService from './utils/auth'
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Home from "./pages/Home";
+import { Routes, Route, BrowserRouter, useNavigate } from "react-router-dom";
+import SignInAndCreateUserContainer from "./components/SignInAndCreateUserContainer";
+import { GlobalProvider } from "./store/GlobalProvider";
+import CustomerTable from "./components/CustomerTable";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import Dashboard from "./pages/Dashboard";
+import NewMember from "./components/NewMember";
+import AddMember from "./pages/AddMember";
+import AuthService from "./utils/auth";
+import Scheduler from "./components/scheduler";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    minHeight: '100vh',
-    backgroundImage: `url(${process.env.PUBLIC_URL + '/images/gym-crm.jpg'})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center center',
-    backgroundAttachment: ' fixed '
+    minHeight: "100vh",
+    // backgroundImage: `url(${process.env.PUBLIC_URL + "/images/gym-crm.jpg"})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+    backgroundAttachment: " fixed ",
   },
 }));
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
-  }
-})
+  };
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
-
 
 export default function App() {
   const classes = useStyles();
@@ -54,8 +58,12 @@ export default function App() {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/login-signup" element={<SignInAndCreateUserContainer />} />
+              <Route
+                path="/login-signup"
+                element={<SignInAndCreateUserContainer />}
+              />
               <Route path="/members" element={<CustomerTable />} />
+              <Route path="/scheduler" element={<Scheduler />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/add-members" element={<AddMember />} />
             </Routes>
--- frontend/src/components/DashboardSideMenu.js
+++ frontend/src/components/DashboardSideMenu.js
@@ -1,101 +1,100 @@
-import React from 'react'
+import React from "react";
 import { PermIdentity, Storefront, Assessment, Work } from "@material-ui/icons";
 import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
 
 const SidebarContainer = styled.div`
-    flex: 1;
-    height: calc(100vh - 50px);
-    background-color: rgb(251, 251, 255);
-    position: sticky;
-    top: 50px;
-`
+  flex: 1;
+  height: calc(100vh - 50px);
+  background-color: rgb(251, 251, 255);
+  position: sticky;
+  top: 50px;
+`;
 const SidebarWrapper = styled.div`
-    padding: 20px;
-    color: #555;
-`
+  padding: 20px;
+  color: #555;
+`;
 const SidebarMenu = styled.div`
-    margin-bottom: 10px;
-`
+  margin-bottom: 10px;
+`;
 const SidebarTitle = styled.h3`
-    font-size: 13px;
-    color: rgb(187, 186, 186);
-`
+  font-size: 13px;
+  color: rgb(187, 186, 186);
+`;
 const SidebarList = styled.ul`
-    list-style: none;
-    padding: 5px;
-`
+  list-style: none;
+  padding: 5px;
+`;
 const SidebarListItem = styled.li`
-    padding: 5px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    border-radius: 10px;
-    &:hover {
-        background-color: rgb(240, 240, 255);
-    }
-`
+  padding: 5px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  border-radius: 10px;
+  &:hover {
+    background-color: rgb(240, 240, 255);
+  }
+`;
 const sharedStyle = css`
-    margin-right: 5px;
-    font-size: 20px !important;
-`
+  margin-right: 5px;
+  font-size: 20px !important;
+`;
 
 const MyPermIdentity = styled(PermIdentity)`
-    ${sharedStyle}
-`
+  ${sharedStyle}
+`;
 const MyStorefront = styled(Storefront)`
-    ${sharedStyle}
-`
+  ${sharedStyle}
+`;
 const MyAssessment = styled(Assessment)`
-    ${sharedStyle}
-`
+  ${sharedStyle}
+`;
 
 const MyWork = styled(Work)`
-    ${sharedStyle}
-`
-
+  ${sharedStyle}
+`;
 
 const Sidebar = () => {
-    return (
-        <SidebarContainer>
-            <SidebarWrapper>
-                <SidebarMenu>
-                    <SidebarTitle>Dashboard</SidebarTitle>
-                    <SidebarList>
-                        <Link to="/members" className="link">
-                            <SidebarListItem>
-                                <MyPermIdentity />
-                                Members
-                            </SidebarListItem>
-                        </Link>
-                        <Link to="/scheduler" className="link">
-                            <SidebarListItem>
-                                <MyStorefront />
-                                Calender
-                            </SidebarListItem>
-                        </Link>
-                        <SidebarListItem>
-                            <MyAssessment />
-                            Reports
-                        </SidebarListItem>
-                    </SidebarList>
-                </SidebarMenu>
-                <SidebarMenu>
-                    <SidebarTitle>Employees</SidebarTitle>
-                    <SidebarList>
-                        <SidebarListItem>
-                            <MyWork />
-                            Manage
-                        </SidebarListItem>
-                        <SidebarListItem>
-                            < MyWork />
-                            Add
-                        </SidebarListItem>
-                    </SidebarList>
-                </SidebarMenu>
-            </SidebarWrapper>
-        </SidebarContainer>
-    )
-}
+  return (
+    <SidebarContainer>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarTitle>Dashboard</SidebarTitle>
+          <SidebarList>
+            <Link to="/members" className="link">
+              <SidebarListItem>
+                <MyPermIdentity />
+                Members
+              </SidebarListItem>
+            </Link>
+            <Link to="/scheduler" className="link">
+              <SidebarListItem>
+                <MyStorefront />
+                Calender
+              </SidebarListItem>
+            </Link>
+            <SidebarListItem>
+              <MyAssessment />
+              Reports
+            </SidebarListItem>
+          </SidebarList>
+        </SidebarMenu>
+        <SidebarMenu>
+          <SidebarTitle>Employees</SidebarTitle>
+          <SidebarList>
+            <SidebarListItem>
+              <MyWork />
+              Manage
+            </SidebarListItem>
+            <SidebarListItem>
+              <MyWork />
+              Add
+            </SidebarListItem>
+          </SidebarList>
+        </SidebarMenu>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
