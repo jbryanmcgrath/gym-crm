ad54c329e2c4cbaf7386447240ea1d446d388dc8
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2022-03-21 20:47:54 -0400

    useranalysis

---
+++ frontend/src/graph-data/data-visualization.js
@@ -0,0 +1,18 @@
+Data-visualization.js
+
+export const memberActivity = 
+
+[
+    { month: 'Jan', membersActiveAtGym: 200, gymMemberships: 400 },
+    { month: 'Feb', membersActiveAtGym: 300, gymMemberships: 401 },
+    { month: 'Mar', membersActiveAtGym: 400, gymMemberships: 500 },
+    { month: 'Apr', membersActiveAtGym: 350, gymMemberships: 500 },
+    { month: 'May', membersActiveAtGym: 400, gymMemberships: 500 },
+    { month: 'June', membersActiveAtGym: 400, gymMemberships: 490 },
+    { month: 'July', membersActiveAtGym: 390, gymMemberships: 490 },
+    { month: 'Aug', membersActiveAtGym: 350, gymMemberships: 480 },
+    { month: 'Sept', membersActiveAtGym: 325, gymMemberships: 480 },
+    { month: 'Oct', membersActiveAtGym: 350, gymMemberships: 480 },
+    { month: 'Nov', membersActiveAtGym: 300, gymMemberships: 480 },
+    { month: 'Dec', membersActiveAtGym: 450, gymMemberships: 500 },
+];
