3e32d473e637436af7e4615c261209ef48e5281b
unknown <dennis11@live.com>
2022-03-28 21:39:37 -0400

    some styling

--- frontend/src/components/DashboardSideMenu.js
+++ frontend/src/components/DashboardSideMenu.js
@@ -1,129 +1,127 @@
-import React from 'react'
+import React from "react";
 import { PermIdentity, Storefront, Assessment, Work } from "@material-ui/icons";
 import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
-import UserAnalysis from '../components/UserAnalysis'
-import { Grid } from '@material-ui/core';
+import UserAnalysis from "../components/UserAnalysis";
+import { Grid } from "@material-ui/core";
 
 const SidebarContainer = styled.div`
-    flex: 1;
-    height: calc(100vh - 50px);
-    background-color: rgb(251, 251, 255);
-    width:calc(15vw);
-    top: 50px;
-`
+  flex: 1;
+  height: calc(100vh - 50px);
+  background-color: rgb(251, 251, 255);
+  width: calc(15vw);
+  top: 50px;
+`;
 const SidebarWrapper = styled.div`
-    padding: 5px;
-    color: #555;
-`
+  padding: 5px;
+  color: #555;
+`;
 const SidebarMenu = styled.div`
-    margin-bottom: 10px;
-`
+  margin-bottom: 10px;
+`;
 const SidebarTitle = styled.h3`
-    font-size: 13px;
-    color: rgb(187, 186, 186);
-`
+  font-size: 13px;
+  color: rgb(187, 186, 186);
+`;
 const SidebarList = styled.ul`
-    list-style: none;
-    padding: 5px;
-`
+  list-style: none;
+  padding: 5px;
+`;
 const SidebarListItem = styled.li`
-    padding: 5px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    border-radius: 10px;
-    &:hover {
-        background-color: rgb(240, 240, 255);
-    }
-`
+  padding: 5px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  border-radius: 10px;
+  &:hover {
+    background-color: rgb(240, 240, 255);
+  }
+`;
 const sharedStyle = css`
-    margin-right: 5px;
-    font-size: 20px !important;
-`
+  margin-right: 5px;
+  font-size: 20px !important;
+`;
 
 const graphSpacing = styled.div`
-    padding: 25px 25px 25px 25px
-    `
+  padding: 25px 25px 25px 25px;
+`;
 
 const MyPermIdentity = styled(PermIdentity)`
-    ${sharedStyle}
-`
+  ${sharedStyle}
+`;
 const MyStorefront = styled(Storefront)`
-    ${sharedStyle}
-`
+  ${sharedStyle}
+`;
 const MyAssessment = styled(Assessment)`
-    ${sharedStyle}
-`
+  ${sharedStyle}
+`;
 
 const MyWork = styled(Work)`
-    ${sharedStyle}
-`
-
+  ${sharedStyle}
+`;
 
 const Sidebar = () => {
-    return (
-        <Grid container>
-            <Grid item md={1}>
-                <SidebarContainer>
-                    <SidebarWrapper>
-                        <SidebarMenu>
-                            <SidebarTitle>Dashboard</SidebarTitle>
-                            <SidebarList>
-                                <Link to="/members" className="link">
-                                    <SidebarListItem>
-                                        <MyPermIdentity />
-                                        Members
-                                    </SidebarListItem>
-                                </Link>
-                                <Link to="/add-members" className="link">
-                                    <SidebarListItem>
-                                        <MyPermIdentity />
-                                        Add Members
-                                    </SidebarListItem>
-                                </Link>
-                                <Link to="/scheduler" className="link">
-                                    <SidebarListItem>
-                                        <MyStorefront />
-                                        Calender
-                                    </SidebarListItem>
-                                </Link>
-                                <Link to="/user-analysis" className='link'>
-                                    <SidebarListItem>
-                                        <MyAssessment />
-                                        Reports
-                                    </SidebarListItem>
-                                </Link>
-                            </SidebarList>
-                        </SidebarMenu>
-                        <SidebarMenu>
-                            <SidebarTitle>Employees</SidebarTitle>
-                            <SidebarList>
-                                <Link to='/employees' className='link'>
-                                    <SidebarListItem>
-                                        <MyWork />
-                                        Manage
-                                    </SidebarListItem>
-                                </Link>
-                                <Link to='/add-employee' className='link'>
-                                    <SidebarListItem>
-                                        < MyPermIdentity />
-                                        Add
-                                    </SidebarListItem>
-                                </Link>
-                            </SidebarList>
-                        </SidebarMenu>
-                    </SidebarWrapper>
-                </SidebarContainer>
-            </Grid>
-            <Grid item md={11} classname="graph-spacing">
-                <analysisContainer>
-                    <UserAnalysis />
-                </analysisContainer>
-            </Grid>
-        </Grid>
+  return (
+    <Grid container>
+      <Grid item md={1}>
+        <SidebarContainer>
+          <SidebarWrapper>
+            <SidebarMenu>
+              <SidebarTitle>Dashboard</SidebarTitle>
+              <SidebarList>
+                <Link to="/members" className="link">
+                  <SidebarListItem>
+                    <MyPermIdentity />
+                    Members
+                  </SidebarListItem>
+                </Link>
+                <Link to="/add-members" className="link">
+                  <SidebarListItem>
+                    <MyPermIdentity />
+                    Add Members
+                  </SidebarListItem>
+                </Link>
+                <Link to="/scheduler" className="link">
+                  <SidebarListItem>
+                    <MyStorefront />
+                    Calender
+                  </SidebarListItem>
+                </Link>
+                <Link to="/user-analysis" className="link">
+                  <SidebarListItem>
+                    <MyAssessment />
+                    Reports
+                  </SidebarListItem>
+                </Link>
+              </SidebarList>
+            </SidebarMenu>
+            <SidebarMenu>
+              <SidebarTitle>Employees</SidebarTitle>
+              <SidebarList>
+                <Link to="/employees" className="link">
+                  <SidebarListItem>
+                    <MyWork />
+                    Manage
+                  </SidebarListItem>
+                </Link>
+                <Link to="/add-employee" className="link">
+                  <SidebarListItem>
+                    <MyPermIdentity />
+                    Add
+                  </SidebarListItem>
+                </Link>
+              </SidebarList>
+            </SidebarMenu>
+          </SidebarWrapper>
+        </SidebarContainer>
+      </Grid>
+      <Grid item md={11} classname="graph-spacing">
+        <AnalysisContainer>
+          <UserAnalysis />
+        </AnalysisContainer>
+      </Grid>
+    </Grid>
+  );
+};
 
-    )
-}
-
-export default Sidebar
+export default Sidebar;
--- frontend/src/components/InfoGraphics.js
+++ frontend/src/components/InfoGraphics.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import ImageCard from './ImageCard';
-import places from '../static/sellingPoints';
-import useWindowPosition from '../hook/useWindowPosition';
-import sellingPoints from '../static/sellingPoints';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import ImageCard from "./ImageCard";
+import places from "../static/sellingPoints";
+import useWindowPosition from "../hook/useWindowPosition";
+import sellingPoints from "../static/sellingPoints";
 const useStyles = makeStyles((theme) => ({
   root: {
-    minHeight: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    [theme.breakpoints.down('md')]: {
-      flexDirection: 'column',
+    minHeight: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
     },
   },
 }));
 export default function () {
   const classes = useStyles();
-  const checked = useWindowPosition('header');
+  const checked = useWindowPosition("header");
   return (
     <div className={classes.root} id="info-graphics">
       <ImageCard place={sellingPoints[0]} checked={checked} />
