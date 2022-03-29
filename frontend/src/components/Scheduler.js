8e10952895f505594ba5f95cd9c5fe2871f33e8e
unknown <dennis11@live.com>
2022-03-28 20:44:55 -0400

    finishing up schedular

--- frontend/src/components/Scheduler.js
+++ frontend/src/components/Scheduler.js
@@ -8,19 +8,29 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { makeStyles } from "@material-ui/core/styles";
+import styled from "styled-components";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        minHeight: "100vh",
-        background: "white",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-        backgroundAttachment: " fixed ",
-        backgroundOpacity: 0.5,
-    },
+  root: {
+    minHeight: "100vh",
+    background: "white",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+    backgroundAttachment: " fixed ",
+    backgroundOpacity: 0.5,
+  },
 }));
 
+const SchedulerContainer = styled.div`
+  background-color: #fff;
+`;
+const DatePickerStyle = styled.div`
+  padding-left: 50px;
+`;
+const Heading = styled.div`
+  text-align: center;
+`;
 
 const locales = {
   "en-US": require("date-fns/locale/en-US"),
@@ -37,18 +47,24 @@ const events = [
   {
     title: "Big Meeting",
     allDay: true,
-    start: new Date(2021, 6, 0),
-    end: new Date(2021, 6, 0),
+    start: new Date(2022, 2, 14),
+    end: new Date(2022, 2, 14),
   },
   {
     title: "Vacation",
-    start: new Date(2021, 6, 7),
-    end: new Date(2021, 6, 10),
+    start: new Date(2022, 2, 7),
+    end: new Date(2022, 2, 10),
   },
   {
     title: "Conference",
-    start: new Date(2021, 6, 20),
-    end: new Date(2021, 6, 23),
+    start: new Date(2022, 2, 20),
+    end: new Date(2022, 2, 24),
+  },
+  {
+    title: "Big Gym Event",
+    allDay: true,
+    start: new Date(2022, 2, 14),
+    end: new Date(2022, 2, 14),
   },
 ];
 
@@ -62,40 +78,50 @@ function Scheduler() {
   }
 
   return (
-    <div className={classes.root}>
-      <h1>Calendar</h1>
-      <h2>Add New Event</h2>
-      <div>
-        <input
-          type="text"
-          placeholder="Add Title"
-          style={{ width: "20%", marginRight: "10px" }}
-          value={newEvent.title}
-          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+    <SchedulerContainer>
+      <div className={classes.root}>
+        <Heading>
+          <h1>Upcoming Events</h1>
+        </Heading>
+        <DatePickerStyle>
+          <h2>Add New Event</h2>
+          <div>
+            <input
+              type="text"
+              placeholder="Add Title"
+              style={{ width: "20%", marginRight: "10px" }}
+              value={newEvent.title}
+              onChange={(e) =>
+                setNewEvent({ ...newEvent, title: e.target.value })
+              }
+            />
+            <DatePicker
+              placeholderText="Start Date"
+              style={{ marginRight: "10px" }}
+              selected={newEvent.start}
+              onChange={(start) => setNewEvent({ ...newEvent, start })}
+            />
+            <DatePicker
+              placeholderText="End Date"
+              selected={newEvent.end}
+              onChange={(end) => setNewEvent({ ...newEvent, end })}
+            />
+            <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+              Add Event
+            </button>
+          </div>
+        </DatePickerStyle>
+
+        <Calendar
+          localizer={localizer}
+          events={allEvents}
+          views={["month"]}
+          startAccessor="start"
+          endAccessor={({ end }) => new Date(end.getTime() + 1)}
+          style={{ height: "60rem", margin: "50px" }}
         />
-        <DatePicker
-          placeholderText="Start Date"
-          style={{ marginRight: "10px" }}
-          selected={newEvent.start}
-          onChange={(start) => setNewEvent({ ...newEvent, start })}
-        />
-        <DatePicker
-          placeholderText="End Date"
-          selected={newEvent.end}
-          onChange={(end) => setNewEvent({ ...newEvent, end })}
-        />
-        <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
-          Add Event
-        </button>
       </div>
-      <Calendar
-        localizer={localizer}
-        events={allEvents}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500, margin: "50px" }}
-      />
-    </div>
+    </SchedulerContainer>
   );
 }
 
